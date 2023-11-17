ba5c349c69985bf31bc9c45aa982192310591f0e
cllo6 <148536808+cllo6@users.noreply.github.com>
2023-11-17 10:37:08 +0000

    Task 4

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-friends = 5
-
+import math
+#----------------------------------------------------------------------------
+#friends = 5
 #friends = friends + 1
 #friends += 1
 #friends = friends - 2
@@ -10,4 +11,32 @@ friends = 5
 #friends /= 2
 #friends = friends ** 2
 
-print(friends)
+#print(friends)
+#----------------------------------------------------------------------------
+# Task 2
+#x = 3.14
+#y = 4 
+#z = 5
+#result = round(x)
+#result = abs(y)
+#result = pow(1,2)
+#result = max(x,y,z)
+#result = min(x,y,z)
+
+#print(result)
+#----------------------------------------------------------------------------
+# Task 3
+# x = 9.1
+
+# print(math.pi)
+# print(math.e)
+# print(math.sqrt(x))
+# print(math.ceil(x))
+# print(math.floor(x))
+#----------------------------------------------------------------------------
+# Task 4
+radius = input('Enter radius: ')
+
+circumference = 2 * math.pi * float(radius)
+
+print(round(circumference), 2)
